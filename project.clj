0ca410ebbda7975d6976c43f02a5392c7d84a6cd
Sam Aaron <samaaron@gmail.com>
2012-12-29 22:55:48 +0000

    update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [aleph "0.3.0-beta7"]
-                 [overtone "0.8.0-SNAPSHOT"]
+                 [overtone "0.8.0-RC13"]
                  [polynome "0.2.2"]
                  [quil "1.6.0"]
                  [compojure "1.1.1"]

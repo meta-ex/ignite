9e4696c02ddf47cfbaa698c1455d4f8967e1ddbe
Sam Aaron <samaaron@gmail.com>
2013-01-28 16:08:45 +0000

    use mixer

--- src/meta_ex/kalk.clj
+++ src/meta_ex/kalk.clj
@@ -1,5 +1,6 @@
 (ns meta-ex.kalk
-  (:use [overtone.core]))
+  (:use [overtone.core]
+        [meta-ex.mixer]))
 
 
 (def snap1 (sample (freesound-path 158615)))
@@ -9,22 +10,18 @@
 (do
   (snap1)
   (snap2))
+
 (do
   (let [n (+ (now) 20)]
     (at n
-        (snap1 :out-bus 10))
+        (snap1 :out-bus (mx)))
     (at (+ n 80)
-        (snap1 :out-bus 10 :vol 0.5) )))
+        (snap1 :out-bus (mx) :vol 0.5) )))
 
 
 (do
   (let [n (+ (now) 20)]
     (at n
-        (snap3 :out-bus 10))
+        (snap3 :out-bus (mx)))
     (at (+ n (+ 30 (rand 10)))
-        (snap3 :out-bus 10 :vol 0.5) )))
-
-
-
-(snap2 :out-bus 10)
-(snap3 :out-bus 10)
+        (snap3 :out-bus (mx)  :vol 0.5) )))

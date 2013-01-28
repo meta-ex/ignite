4264fffedd3b581cd55737ecfffc2ee94afc5bc4
Sam Aaron <samaaron@gmail.com>
2013-01-28 16:11:59 +0000

    timing triggers are now part of overtone

--- src/meta_ex/triggers.clj
+++ src/meta_ex/triggers.clj
@@ -1,14 +1,6 @@
 (ns meta-ex.triggers
-  (:use [overtone.live]))
-
-(defsynth trigger [rate 100 out-bus 0]
-  (out:kr out-bus (impulse:kr rate)))
-
-(defsynth counter [in-trg-bus 0 out-cnt-bus 0]
-  (out:kr out-cnt-bus (pulse-count:kr (in:kr in-trg-bus))))
-
-(defsynth divider [div 32 in-trg-bus 0 out-trg-bus 0]
-  (out:kr out-trg-bus (pulse-divider (in:kr in-trg-bus) div)))
+  (:use [overtone.live]
+        [overtone.synth.timing]))
 
 (defonce triggers-g (group "M-x triggers" :tgt (foundation-safe-pre-default-group)))
 
@@ -17,5 +9,5 @@
 (defonce cnt-b (control-bus))
 
 (defonce trigger-s (trigger :tgt triggers-g :out-bus trg-b))
-(defonce divider-s (divider :tgt triggers-g :in-trg-bus trg-b :out-trg-bus beat-b))
-(defonce counter-s (counter :tgt triggers-g :in-trg-bus beat-b :out-cnt-bus cnt-b))
+(defonce divider-s (divider :tgt triggers-g :in-bus trg-b :out-bus beat-b))
+(defonce counter-s (counter :tgt triggers-g :in-bus beat-b :out-bus cnt-b))

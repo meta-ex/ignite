e044b4ad1e1072bd0e3d010e531c1294cbacac15
Sam Aaron <samaaron@gmail.com>
2013-04-28 20:10:31 +0100

    update dependency to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [aleph "0.3.0-beta7"]
                  [overtone "0.8.1"]
                  [polynome "0.3.0-SNAPSHOT"]

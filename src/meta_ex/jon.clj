4ebba18d19451ce7fe534ad84892268e4d01206b
Sam Aaron <samaaron@gmail.com>
2013-08-05 00:16:49 +0100

    teach jon ns to handle lack of connected monomers

--- src/meta_ex/jon.clj
+++ src/meta_ex/jon.clj
@@ -8,18 +8,32 @@
             [meta-ex.kit.sampler :as samp]))
 
 (defn swap-samples-128 [samps]
-  (ms/swap-samples! drums/seq128 samps)
-  :swapped-128)
+  (if (bound? #'drums/seq128)
+    (do
+      (ms/swap-samples! drums/seq128 samps)
+      :swapped-128)
+    :monome-128-not-found))
 
 (defn swap-samples-64 [samps]
-  (ms/swap-samples! drums/seq64 samps)
-  :swapped-64)
+  (if (bound? #'drums/seq64)
+    (do
+      (ms/swap-samples! drums/seq64 samps)
+      :swapped-64)
+    :monome-64-not-found))
 
 (defn swap-trigs-128 [samps]
-  (samp/swap-samples! drums/trigger-sampler128 samps))
+  (if (bound? #'drums/trigger-sampler128)
+    (do
+      (samp/swap-samples! drums/trigger-sampler128 samps)
+      :swapped-trig-128)
+    :trig-128-not-found))
 
 (defn swap-trigs-64 [samps]
-  (samp/swap-samples! drums/trigger-sampler64 samps))
+  (if (bound? #'drums/trigger-sampler64)
+    (do
+      (samp/swap-samples! drums/trigger-sampler64 samps)
+      :swapped-trig-64)
+    :trig-64-not-found))
 
 (defn rate [r]
   (ctl tim/root-s :rate r))

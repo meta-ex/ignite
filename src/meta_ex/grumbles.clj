c33fcdb1e9710287026e9e50ae8c9854e27900c9
Sam Aaron <samaaron@gmail.com>
2013-08-04 09:30:46 +0100

    play around with grumbles

--- src/meta_ex/grumbles.clj
+++ src/meta_ex/grumbles.clj
@@ -10,7 +10,7 @@
                           (max 0 (+ (lf-noise1:kr (lag speed 60))
                                     (line:kr 1 -1 30 :action FREE))))
                       [1 (/ 2 3) (/ 3 2) 2]))]
-    (out out-bus  (* amp (pan2 snd (sin-osc:kr 50))) )))
+    (out out-bus  (* amp  (pan2 snd (sin-osc:kr 50))) )))
 
 (defsynth grumble [speed 6 freq-mul 1 out-bus 0 amp 1]
   (let [snd (mix (map #(* (square (* % freq-mul 100))
@@ -25,24 +25,24 @@
 (def ob 0)
 (volume 0.55)
 
-(grumble [:head grumble-g] :freq-mul 2 :out-bus ob :amp 1)
-(grumble [:head grumble-g] :freq-mul 1.8 :out-bus ob :amp 1)
-(grumble [:head grumble-g] :freq-mul 1.5 :out-bus ob :amp 1)
+(grumble [:head grumble-g] :freq-mul 2 :out-bus ob :amp 2)
+(grumble [:head grumble-g] :freq-mul 1.8 :out-bus ob :amp 2)
+(grumble [:head grumble-g] :freq-mul 1.5 :out-bus ob :amp 2)
 
 (do  (grumble [:head grumble-g] :freq-mul 1 :out-bus ob :amp 1)
   (grumble [:head grumble-g] :freq-mul 0.5 :out-bus ob :amp 1))
 
 (do
-  (grumble [:head grumble-g] :freq-mul 1 :out-bus 0 :amp 3)
-  (grumble [:head grumble-g] :freq-mul 0.5 :out-bus 0 :amp 2))
+  (grumble [:head grumble-g] :freq-mul 1 :out-bus ob :amp 3)
+  (grumble [:head grumble-g] :freq-mul 0.5 :out-bus ob :amp 2))
 (ctl grumble-g :speed 1997)
 
 (defn sin-ctl
   [ctl-id arg-map]
   (reduce (fn [res [k v]]
-            (let [idx (mix-idx k)]
+            (let [idx (synth-arg-index meta-mix k)]
               (merge res (map (fn [[k v]]
-                                [(keyword (str (name k) "-" mix-idx)) v])
+                                [(keyword (str (name k) "-" idx)) v])
                               v))))
           {}
           arg-map))
@@ -53,7 +53,7 @@
                 :add 0.5}})
 
 (ctl  (nkmx-sctl :s1)
-     :freq-mul-7 1
+     :freq-mul-7 0
      :mul-7 1
      :add-7 0.5)
 
@@ -61,3 +61,4 @@
      :freq-mul-13 1/8
      :mul-13 1
      :add-13 0.5)
+;;(status)

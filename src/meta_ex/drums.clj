a02f6a92092234d4f9239208b3a9d97ed3cd4047
Sam Aaron <samaaron@gmail.com>
2013-08-07 13:46:11 +0100

    comment out debug aid

--- src/meta_ex/drums.clj
+++ src/meta_ex/drums.clj
@@ -120,7 +120,7 @@
 
 ;;(ms/stop-sequencer seq128)
 ;;(ms/stop-sequencer seq64)
-  (.printStackTrace (agent-error (:state (:fonome seq128))))
+;;  (.printStackTrace (agent-error (:state (:fonome seq128))))
 ;; (def c-sequencer (seq/mk-sequencer "m128" african-samples 16 drum-g tim/beat-b tim/beat-count-b 0))
 ;; (def c-sequencer4 (seq/mk-sequencer "yo5" orig-samples 8 drum-g tim/beat-b tim/beat-count-b 0))
 

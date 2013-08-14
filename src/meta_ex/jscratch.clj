c0032f5697ac2a5c75d47b2492e12a693e8e0553
Sam Aaron <samaaron@gmail.com>
2013-08-14 22:36:59 +0100

    having fun with jscratch

--- src/meta_ex/jscratch.clj
+++ src/meta_ex/jscratch.clj
@@ -12,6 +12,8 @@
 (swap-samples-128 bleep-samples)
 (swap-samples-128 bleep1-samples)
 (swap-samples-128 bleep2-samples)
+(swap-samples-128 bleep3-samples)
+(swap-samples-128 bleep4-samples)
 (swap-samples-128 clapkick1-samples)
 (swap-samples-128 clapkick2-samples)
 (swap-samples-128 kicks-samples)
@@ -25,11 +27,44 @@
 (swap-trigs-128 ambient-drum-samples)
 (swap-trigs-128 trigger-samples)
 (swap-trigs-128 atmos-samples)
+(swap-trigs-128 bleep3-samples)
 
 (swap-trigs-64 trigger-samples)
 
+(data-riff-load-bass [:C4 :E4 :G4 :E4 :C5 :E4 :G4 :E4])
+(bass-rate 1/256)
+(bass-wob-rate 1)
+(bass-amp 1)
+(hi-amp 1)
+(mid-amp 1)
+(mid-hi-rate 1/16)
+(data-riff-load-mid-hi [:C4 :E4 :G4 :E4 :C5 :E4 :G4 :D5 :C5 :G4 :C5 :D5 :C5 :E4 :G4 :D4]-12)
+(data-riff-load-mid-hi [:C4 :C4 :C4 :C4 :C5 :E4 :G4 :D5 :C5 :G4 :C5 :D5 :C5 :E4 :G4 :D4]-12)
+(data-riff-load-mid-hi [:C4 :C4 :C4 :C4 :C5 :E4 :G4 :D5 :C5 :C5 :C5 :D5 :C5 :E4 :G4 :D4]-12)
+(data-riff-load-mid-hi [:C4 :C4 :E4 :E4 :G4 :G4 :E4 :E4 :C5 :C5 :E4 :E4 :G4 :G4 :D4 :D4]-12)
+(giorgio 2)
+(bass-map-keyboard-on)
+(bass-map-keyboard-off)
 
-
+(do
+  (hi-amp 0)
+  (mid-amp 0)
+  (bass-amp 1)
+  )
 (rate 4)
 
 (status)
+
+(def bleep3-samples
+  [(freesound 64072)
+   (freesound 25882)
+   (freesound 74233)
+   (freesound 70106)
+   (freesound 86101)])
+
+(def bleep4-samples
+  [(freesound 86101)
+   (freesound 64072)
+   (freesound 25882)
+   (freesound 74233)
+   (freesound 70106)])

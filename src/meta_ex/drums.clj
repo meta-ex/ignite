71c580c5ba4442ac10344bd9c071a82af306052a
Sam Aaron <samaaron@gmail.com>
2013-08-17 15:38:05 +0100

    start basic mixer on same amp as vnk2

--- src/meta_ex/drums.clj
+++ src/meta_ex/drums.clj
@@ -23,7 +23,7 @@
 
 (do
 
-  (defsynth basic-mixer [boost 0 amp 1 mute 1 in-bus 0 out-bus 0 clamp-down-t 0.05]
+  (defsynth basic-mixer [boost 0 amp 0.5 mute 1 in-bus 0 out-bus 0 clamp-down-t 0.05]
     (out out-bus (* (+ boost 1) amp (lag mute clamp-down-t) (in:ar in-bus 2))))
 
   (defonce drum-g             (group))

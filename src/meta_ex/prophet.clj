d3ae456d4d6bf0f87eb06e49212d9e88d29f4b03
Sam Aaron <samaaron@gmail.com>
2014-04-08 23:27:00 +0100

    Fiddling with the prophet synth and beat buffers

--- src/meta_ex/prophet.clj
+++ src/meta_ex/prophet.clj
@@ -1,6 +1,6 @@
 (ns meta-ex.prophet
   (:use [overtone.live]
-
+        [meta-ex.keyboard]
         [meta-ex.kit.mixer])
   (:require [meta-ex.kit.timing :as tim]))
 
@@ -21,9 +21,11 @@
    frequency) and its LFO has a little bit of randomisation thrown into
    its frequency component for that extra bit of variety."
 
-  [amp 1 freq 440 slide 2 cutoff-freq 12000 rq 0.3  attack 1 decay 2 out-bus 0 beat-b (:id tim/inv-root-b) beat-cnt-b (:id tim/count-b) rhyth-bf 0 rhyth-bf-size 16]
+  [amp 1 note 52 note-mod 0 slide 0 cutoff-freq 12000 cutoff-slide 2 rq 0.3  attack 1 decay 2 out-bus 0 beat-b (:id tim/inv-root-b) beat-cnt-b (:id tim/count-b) rhyth-bf 0 rhyth-bf-size 16]
 
-  (let [freq (lag freq slide)
+  (let [note (+ note note-mod)
+        freq (midicps note)
+        freq (lag freq slide)
         snd  (pan2 (mix [(pulse freq (* 0.1 (/ (+ 1.2 (sin-osc:kr 1)) )))
                          (pulse freq (* 0.8 (/ (+ 1.2 (sin-osc:kr 0.3) 0.7) 2)))
                          (pulse freq (* 0.8 (/ (+ 1.2 (lf-tri:kr 0.4 )) 2)))
@@ -36,64 +38,51 @@
         beat (* (in:kr beat-b)
                 (buf-rd:kr 1 rhyth-bf cnt))
         env  (env-gen (perc attack decay) :gate beat)
-        snd  (rlpf (* env snd snd) (lag cutoff-freq 0.3) rq)]
+        snd  (rlpf (* env snd snd) (lag cutoff-freq cutoff-slide) rq)]
 
     (out out-bus (* amp snd))))
+
+(do
+  (defonce rhyth (buffer 16))
+  (defonce proph-g (group "The Prophet" ))
+
+  (def p4 (prophet2 [:head proph-g] :note-mod -24 :amp 0 :out-bus (nkmx :s0)) )
+  (def p5 (prophet2 [:head proph-g] :note-mod 0 :amp 0 :out-bus (nkmx :s0)))
+  (def p6  (prophet2 [:head proph-g] :note-mod -12 :amp 0 :out-bus (nkmx :s0)))
+
+  (on-event (sam-kb :note-on)
+            (fn [m] (ctl proph-g :note (:note m)))
+            ::proph-control)
+
+  (buffer-write! rhyth (rotate 1 [1 0 1 0 1 0 1 0 1 0 1 0 1 0 0 1]))
+  )
+
+(let [d 0.5
+      a 0.01
+      rq 0.5
+      bb tim/beat-2th
+      bb2 tim/beat-4th
+      bb3 tim/beat-6th
+      bb3 bb3
+      bb2 bb2
+      bb bb
+      amp 3
+      cf 100
+      n (note :a3)]
+
+  (ctl p4 :note-mod 5 :cutoff-freq 3000 :decay 0.1  :beat-b (:beat bb2) :beat-cnt-b (:count bb2) :amp 0)
+  (ctl p5 :note-mod 12 :decay 0.2  :beat-b (:beat bb3) :beat-cnt-b (:count bb3) :amp 0)
+  (ctl p6 :note-mod -24 :decay 0.5 :amp 20 :cutoff-freq 100 :beat-b (:beat bb) :beat-cnt-b (:count bb)  :amp 0.1))
+
+(kill proph-g)
 
 
 (comment
   (rotate -2 [1 2 3 4 5])
 
-  (def rhyth (buffer 16))
+  (defonce rhyth (buffer 16))
   (buffer-write! rhyth [1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0])
   (buffer-write! rhyth [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])
   (buffer-write! rhyth [1 1 1 1 1 1 1 1 0 1 1 1 1 0 1 1])
   (def bb (tim/beat-bus 1/16))
-
-
-  (defonce proph-g (group "The Prophet" ))
-
-  (let [n  (note :a1)
-        d 10
-        rq 0.6
-        a 3
-        amp 0.5
-        cf 1000]
-    (def p  (prophet2 [:head proph-g] :freq (midi->hz n)  :decay d :rq rq :attack a :cutoff-freq cf :beat-b (:beat bb) :beat-cnt-b (:count bb) :rhyth-bf rhyth :out-bus (nkmx :s1)) )
-    (def p2 (prophet2 [:head proph-g] :freq (midi->hz (- n 12)) :decay d :rq rq :attack a :cutoff-freq cf :beat-b (:beat bb) :beat-cnt-b (:count bb) :rhyth-bf rhyth :out-bus (nkmx :s1)))
-    (def p3  (prophet2 [:head proph-g] :freq (midi->hz (+ n 7)) :decay d :rq rq :attack a :cutoff-freq cf :beat-b (:beat bb) :beat-cnt-b (:count bb) :rhyth-bf rhyth :out-bus (nkmx :s1)))
-    )
-
-  )
-(volume 0.5)
-
-(let [d 0.2
-      a 0.01
-      rq 0.9
-      bb tim/beat-2th
-      amp 3
-      cf 1000]
-  (ctl proph-g :cutoff-freq cf :amp amp :decay d :attack a :rq 0.6 :beat-b (:beat bb) :beat-cnt-b (:count bb)))
-
-(on-event [:midi :note-on]
-          (fn [msg]
-
-            (let [n (:note msg)
-                  f (midi->hz n)
-                  f2 (midi->hz (- n 12))
-                  f3 (midi->hz (+ 7 n))]
-              (ctl p :freq f :out-bus (nkmx :m0))
-              (ctl p2 :freq f2 :out-bus (nkmx :m0))
-              (ctl p3 :freq f3 :out-bus (nkmx :m0))))
-          ::phoph-control)
-
-
-
-
-(kill p p2 p3)
-(stop)
-(kill proph-g)
-
-(do
-
 )

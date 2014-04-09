921f5a484e25da66f327ff12d00f0e52eb78caa4
Sam Aaron <samaaron@gmail.com>
2014-04-09 15:12:17 +0100

    more prophet mangling

--- src/meta_ex/prophet.clj
+++ src/meta_ex/prophet.clj
@@ -21,7 +21,7 @@
    frequency) and its LFO has a little bit of randomisation thrown into
    its frequency component for that extra bit of variety."
 
-  [amp 1 note 52 note-mod 0 slide 0 cutoff-freq 12000 cutoff-slide 2 rq 0.3  attack 1 decay 2 out-bus 0 beat-b (:id tim/inv-root-b) beat-cnt-b (:id tim/count-b) rhyth-bf 0 rhyth-bf-size 16]
+  [amp 1 note 52 note-mod 0 slide 0 cutoff-freq 12000 cutoff-slide 2 rq 0.3  attack 1 release 2 out-bus 0 beat-b (:id tim/inv-root-b) beat-cnt-b (:id tim/count-b) rhyth-bf 0 rhyth-bf-size 16]
 
   (let [note (+ note note-mod)
         freq (midicps note)
@@ -37,7 +37,7 @@
                   rhyth-bf-size)
         beat (* (in:kr beat-b)
                 (buf-rd:kr 1 rhyth-bf cnt))
-        env  (env-gen (perc attack decay) :gate beat)
+        env  (env-gen (perc attack release) :gate beat)
         snd  (rlpf (* env snd snd) (lag cutoff-freq cutoff-slide) rq)]
 
     (out out-bus (* amp snd))))
@@ -63,26 +63,22 @@
       bb tim/beat-2th
       bb2 tim/beat-4th
       bb3 tim/beat-6th
-      bb3 bb3
-      bb2 bb2
+      bb3 bb
+      bb2 bb
       bb bb
       amp 3
       cf 100
       n (note :a3)]
 
-  (ctl p4 :note-mod 5 :cutoff-freq 3000 :decay 0.1  :beat-b (:beat bb2) :beat-cnt-b (:count bb2) :amp 0)
-  (ctl p5 :note-mod 12 :decay 0.2  :beat-b (:beat bb3) :beat-cnt-b (:count bb3) :amp 0)
-  (ctl p6 :note-mod -24 :decay 0.5 :amp 20 :cutoff-freq 100 :beat-b (:beat bb) :beat-cnt-b (:count bb)  :amp 0.1))
+  (ctl p4 :note-mod 5 :cutoff-freq 1000 :release 0.1  :beat-b (:beat bb2) :beat-cnt-b (:count bb2) :amp 0.5)
+  (ctl p5 :note-mod 0 :release 0.1 :beat-b (:beat bb3) :beat-cnt-b (:count bb3) :amp 0.4)
+  (ctl p6 :note-mod -24 :release 0.2 :cutoff-freq 2000 :beat-b (:beat bb) :beat-cnt-b (:count bb)  :amp 0))
 
 (kill proph-g)
 
 
 (comment
   (rotate -2 [1 2 3 4 5])
-
-  (defonce rhyth (buffer 16))
   (buffer-write! rhyth [1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0])
   (buffer-write! rhyth [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])
-  (buffer-write! rhyth [1 1 1 1 1 1 1 1 0 1 1 1 1 0 1 1])
-  (def bb (tim/beat-bus 1/16))
-)
+  (buffer-write! rhyth [1 1 1 1 1 1 1 1 0 1 1 1 1 0 1 1]))
